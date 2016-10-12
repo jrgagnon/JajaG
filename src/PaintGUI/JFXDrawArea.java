88655c50c071facbae28bb8431fdeeb1909dc8c2
jrgagnon <jrgagnon@mtu.edu>
2016-10-12 14:57:36 -0400

    Working JFX Panel, added new draw class to be used with it

---
+++ src/PaintGUI/JFXDrawArea.java
@@ -0,0 +1,130 @@
+package PaintGUI;
+
+import java.awt.Color;
+import java.awt.Graphics;
+import java.awt.Graphics2D;
+import java.awt.Image;
+import java.awt.RenderingHints;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseMotionAdapter;
+import java.awt.image.BufferedImage;
+
+import javax.swing.JComponent;
+@SuppressWarnings("serial")
+public class JFXDrawArea extends JComponent{
+
+	// image were going to draw
+	private BufferedImage image;
+	// used to draw on
+	private Graphics2D g2;
+	// Mouse coordinates
+	private int currentX, currentY, oldX, oldY;
+
+	/**
+	 * Cases for the tool variable 0: Draw Line 1: Rectangle 2: Circle 3: Eraser
+	 */
+	public int tool = 0;
+
+	public DrawArea() {
+		draw();
+	}
+	
+	protected void draw(){
+		System.out.println(tool);
+
+		if (tool == 0) { // Draw Line
+			setDoubleBuffered(false);
+			addMouseListener(new MouseAdapter() {
+				public void mousePressed(MouseEvent e) {
+					// save x,y when mouse pressed
+					oldX = e.getX();
+					oldY = e.getY();
+				}
+
+			});
+
+			addMouseMotionListener(new MouseMotionAdapter() {
+				public void mouseDragged(MouseEvent e) {
+					// coord x,y when mouse is dragged
+					currentX = e.getX();
+					currentY = e.getY();
+
+					if (g2 != null) {
+						// draw line
+						g2.drawLine(oldX, oldY, currentX, currentY);
+						// refresh draw area to paint
+						repaint();
+						// store current coords x,y
+						oldX = currentX;
+						oldY = currentY;
+					}
+				}
+
+			});
+		} else if (tool == 1) {// Draw Rect
+
+		} else if (tool == 2) {// Draw Circle
+
+		} else if(tool == 3){//Erase
+			
+		}
+	}
+
+	protected void paintComponent(Graphics g) {
+		if (image == null) {
+			// image to draw null => create
+			image = (BufferedImage) createImage(getSize().width, getSize().height);
+			g2 = (Graphics2D) image.getGraphics();
+			// enable
+			g2.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
+			// clear draw area
+			clear();
+		}
+
+		g.drawImage(image, 0, 0, null);
+	}
+
+	// now we create exposed methods
+
+	public void clear() {
+		g2.setPaint(Color.white);
+		// draw white on entire area
+		g2.fillRect(0, 0, getSize().width, getSize().height);
+		g2.setPaint(Color.black);
+		repaint();
+	}
+
+	public void red() {
+		// apply red color on g2 context
+		g2.setPaint(Color.red);
+	}
+
+	public void black() {
+		g2.setPaint(Color.black);
+	}
+
+	public void magenta() {
+		g2.setPaint(Color.magenta);
+	}
+
+	public void green() {
+		g2.setPaint(Color.green);
+	}
+
+	public void blue() {
+		g2.setPaint(Color.blue);
+	}
+
+	public void yellow() {
+		g2.setPaint(Color.yellow);
+	}
+
+	public void grey() {
+		g2.setPaint(Color.gray);
+	}
+
+	public void orange() {
+		g2.setPaint(Color.orange);
+	}
+}
--- src/PaintGUI/JFXPaint.java
+++ src/PaintGUI/JFXPaint.java
@@ -1,8 +1,100 @@
 package PaintGUI;
+
+import javafx.application.Application;
+import javafx.beans.value.ChangeListener;
+import javafx.beans.value.ObservableValue;
+import javafx.geometry.Insets;
+import javafx.scene.Group;
+import javafx.scene.Scene;
+import javafx.scene.control.Label;
+import javafx.scene.control.Toggle;
+import javafx.scene.control.ToggleButton;
+import javafx.scene.control.ToggleGroup;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.VBox;
+import javafx.scene.paint.Color;
+import javafx.scene.shape.Rectangle;
+import javafx.stage.Stage;
+
 /**
  * Used to intialize JFX components currently for testing purposes
+ * 
  * @author Jordan Gagnon
  */
-public class JFXPaint {
+public class JFXPaint extends Application {
+
+	ImageView image = new ImageView();
+	Rectangle rect = new Rectangle(700, 400);
+	private static final Label label = new Label("Tools:");
+
+	public static void main(String[] args) {
+		launch(args);
+	}
+
+	@Override
+	public void start(Stage stage) {
+		Scene scene = new Scene(new Group());
+		stage.setTitle("JFXPaint");
+		stage.setWidth(800);
+		stage.setHeight(600);
+
+		rect.setFill(Color.WHITE);
+		rect.setStroke(Color.DARKGRAY);
+		rect.setStrokeWidth(2);
+
+		final ToggleGroup group = new ToggleGroup();
+
+		group.selectedToggleProperty().addListener(new ChangeListener<Toggle>() {
+			public void changed(ObservableValue<? extends Toggle> ov, Toggle toggle, Toggle new_toggle) {
+				if (new_toggle == null)
+					rect.setFill(Color.WHITE);
+				else
+					rect.setFill((Color) group.getSelectedToggle().getUserData());
+			}
+		});
+
+		ToggleButton drawLineBtn = new ToggleButton("Line");
+		drawLineBtn.setToggleGroup(group);
+		drawLineBtn.setUserData(Color.LIGHTGREEN);
+		drawLineBtn.setSelected(true);
+		drawLineBtn.setStyle("-fx-base: lightgreen;");
+
+		ToggleButton rectButton = new ToggleButton("Rect");
+		rectButton.setToggleGroup(group);
+		rectButton.setUserData(Color.LIGHTBLUE);
+		rectButton.setStyle("-fx-base: lightblue;");
+
+		ToggleButton circleBtn = new ToggleButton("Circle");
+		circleBtn.setToggleGroup(group);
+		circleBtn.setUserData(Color.PLUM);
+		circleBtn.setStyle("-fx-base: plum;");
+		
+		ToggleButton eraserBtn = new ToggleButton("Eraser");
+		eraserBtn.setToggleGroup(group);
+		eraserBtn.setUserData(Color.SALMON);
+		eraserBtn.setStyle("-fx-base: salmon;");
+
+		HBox hbox = new HBox();
+
+		hbox.getChildren().add(drawLineBtn);
+		hbox.getChildren().add(rectButton);
+		hbox.getChildren().add(circleBtn);
+		hbox.getChildren().add(eraserBtn);
+
+		rect.setArcHeight(10);
+		rect.setArcWidth(10);
+
+		VBox vbox = new VBox();
+
+		vbox.getChildren().add(label);
+		vbox.getChildren().add(hbox);
+		vbox.getChildren().add(rect);
+		vbox.setPadding(new Insets(20, 10, 10, 20));
+
+		((Group) scene.getRoot()).getChildren().add(vbox);
+		stage.setScene(scene);
+		stage.show();
+	}
 
 }
