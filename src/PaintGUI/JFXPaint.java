fc873c00d99ebd778bd769f0c77b702bcef44be9
jrgagnon <jrgagnon@mtu.edu>
2016-10-12 16:50:00 -0400

    Working JFX App, Properly imports JComponent has working toggle buttons

--- src/PaintGUI/JFXPaint.java
+++ src/PaintGUI/JFXPaint.java
@@ -3,6 +3,7 @@ package PaintGUI;
 import javafx.application.Application;
 import javafx.beans.value.ChangeListener;
 import javafx.beans.value.ObservableValue;
+import javafx.embed.swing.SwingNode;
 import javafx.geometry.Insets;
 import javafx.scene.Group;
 import javafx.scene.Scene;
@@ -10,11 +11,8 @@ import javafx.scene.control.Label;
 import javafx.scene.control.Toggle;
 import javafx.scene.control.ToggleButton;
 import javafx.scene.control.ToggleGroup;
-import javafx.scene.image.ImageView;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.VBox;
-import javafx.scene.paint.Color;
-import javafx.scene.shape.Rectangle;
 import javafx.stage.Stage;
 
 /**
@@ -24,8 +22,6 @@ import javafx.stage.Stage;
  */
 public class JFXPaint extends Application {
 
-	ImageView image = new ImageView();
-	Rectangle rect = new Rectangle(700, 400);
 	private static final Label label = new Label("Tools:");
 
 	public static void main(String[] args) {
@@ -34,45 +30,70 @@ public class JFXPaint extends Application {
 
 	@Override
 	public void start(Stage stage) {
+
+		// Imports the JComponent Draw Area into the JFX Window
+		SwingNode swingNode = new SwingNode();
+		JFXDrawArea drawArea = new JFXDrawArea();
+		swingNode.setContent(drawArea);
+
 		Scene scene = new Scene(new Group());
 		stage.setTitle("JFXPaint");
 		stage.setWidth(800);
 		stage.setHeight(600);
 
-		rect.setFill(Color.WHITE);
-		rect.setStroke(Color.DARKGRAY);
-		rect.setStrokeWidth(2);
+		// Group that Contains all the toggle buttons, only one or no buttons in
+		// this group can be selected
+		final ToggleGroup tools = new ToggleGroup();
 
-		final ToggleGroup group = new ToggleGroup();
-
-		group.selectedToggleProperty().addListener(new ChangeListener<Toggle>() {
+		tools.selectedToggleProperty().addListener(new ChangeListener<Toggle>() {
 			public void changed(ObservableValue<? extends Toggle> ov, Toggle toggle, Toggle new_toggle) {
-				if (new_toggle == null)
-					rect.setFill(Color.WHITE);
-				else
-					rect.setFill((Color) group.getSelectedToggle().getUserData());
+				if (new_toggle == null) {
+					// Do Nothing on no change
+				} else {
+					
+					int tool = (int) tools.getSelectedToggle().getUserData();
+					switch (tool) {
+					case 0:
+						drawArea.drawLineTool();
+						break;
+					case 1:
+						drawArea.rectTool();
+						break;
+					case 2:
+						drawArea.circleTool();
+						break;
+					case 3:
+						drawArea.eraserTool();
+						break;
+					default:
+						System.out.println("Default");
+						break;
+
+					}
+				}
+
 			}
 		});
 
 		ToggleButton drawLineBtn = new ToggleButton("Line");
-		drawLineBtn.setToggleGroup(group);
-		drawLineBtn.setUserData(Color.LIGHTGREEN);
+		drawLineBtn.setToggleGroup(tools);
+		drawLineBtn.setUserData(0);
 		drawLineBtn.setSelected(true);
 		drawLineBtn.setStyle("-fx-base: lightgreen;");
 
 		ToggleButton rectButton = new ToggleButton("Rect");
-		rectButton.setToggleGroup(group);
-		rectButton.setUserData(Color.LIGHTBLUE);
+		rectButton.setToggleGroup(tools);
+		rectButton.setUserData(1);
 		rectButton.setStyle("-fx-base: lightblue;");
 
 		ToggleButton circleBtn = new ToggleButton("Circle");
-		circleBtn.setToggleGroup(group);
-		circleBtn.setUserData(Color.PLUM);
+		circleBtn.setToggleGroup(tools);
+		circleBtn.setUserData(2);
 		circleBtn.setStyle("-fx-base: plum;");
-		
+
 		ToggleButton eraserBtn = new ToggleButton("Eraser");
-		eraserBtn.setToggleGroup(group);
-		eraserBtn.setUserData(Color.SALMON);
+		eraserBtn.setToggleGroup(tools);
+		eraserBtn.setUserData(3);
 		eraserBtn.setStyle("-fx-base: salmon;");
 
 		HBox hbox = new HBox();
@@ -82,14 +103,11 @@ public class JFXPaint extends Application {
 		hbox.getChildren().add(circleBtn);
 		hbox.getChildren().add(eraserBtn);
 
-		rect.setArcHeight(10);
-		rect.setArcWidth(10);
-
 		VBox vbox = new VBox();
 
 		vbox.getChildren().add(label);
 		vbox.getChildren().add(hbox);
-		vbox.getChildren().add(rect);
+		vbox.getChildren().add(swingNode);
 		vbox.setPadding(new Insets(20, 10, 10, 20));
 
 		((Group) scene.getRoot()).getChildren().add(vbox);
